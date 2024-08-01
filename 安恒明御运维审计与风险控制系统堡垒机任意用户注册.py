fde7e470a414a0559445e66e50d3a4f3610b5a19
wsgdr7 <3074394329@qq.com>
2024-08-01 20:24:31 +0800

    Update 安恒明御运维审计与风险控制系统堡垒机任意用户注册.py

--- 安恒明御运维审计与风险控制系统堡垒机任意用户注册.py
+++ 安恒明御运维审计与风险控制系统堡垒机任意用户注册.py
@@ -25,10 +25,22 @@ def banner():
 def main():
     banner()
     parser = argparse.ArgumentParser(description='安恒明御运维审计与风险控制系统堡垒机任意用户注册')
-    parser.add_argument('-url','--url',dest='url',type=str,help='input your url')
+    parser.add_argument('-u','--url',dest='url',type=str,help='Please Input URL')
+    parser.add_argument('-f','--file',dest='file',type=str,help='Please Input File')
     args = parser.parse_args()
-    if args.url:
-        poc()
+    if args.url and not args.file:
+        poc(args.url)
+    elif args.file and not args.url:
+        url_list =[]
+        with open(args.file,'r',encoding='utf-8') as fp:
+            for i in fp.readlines():
+                url_list.append(i.strip())
+        pool = Pool(80)
+        pool.map(poc,url_list)
+        pool.close()
+        pool.join()
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h")
 
 #poc函数
 def poc(target):
