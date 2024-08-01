65ec751362c1d6edfd6a845e7e04e29860a49ecb
wsgdr7 <3074394329@qq.com>
2024-08-01 19:55:31 +0800

    Update Adobe ColdFusion ---Deserialize.py

--- Adobe ColdFusion ---Deserialize.py
+++ Adobe ColdFusion ---Deserialize.py
@@ -24,9 +24,19 @@ def main():
     parse = argparse.ArgumentParser(description="Adobe ColdFusion反序列化漏洞")
     #添加参数
     parse.add_argument('-u','--url',dest='url',type=str,help="please input url")
+    parse.add_argument('-f','--file',dest='file',type=str,help="please input file")
     args = parse.parse_args()
-    if args.url:
+    if args.url and not args.file:
         poc(args.url)
+    elif args.file and not args.url:
+        url_list=[]
+        with open(args.file,'r',encoding='utf-8') as fp:
+            for i in fp.readlines():
+                url_list.append(i.strip())
+        pool = Pool(80)
+        pool.map(poc,url_list)
+        pool.close()
+        pool.join()
     else:
         print(f"Usage:\n\t python3 {sys.argv[0]} -h")
 
